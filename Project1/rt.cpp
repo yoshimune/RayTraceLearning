72359a500cc10d704fb903588d713bff9bb02cd6
Yoshimune <seatangle.dash@gmail.com>
2018-06-13 23:38:43 +0900

    add cornell box

--- Project1/rt.cpp
+++ Project1/rt.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -55,6 +56,7 @@ V normarize(V v) {
 //Tone Map
 int tonemap(double v) {
 	// ガンマ補正をかけてから 0 - 255 でクランプする
+	v = std::abs(v);
 	return std::min(std::max(int(std::pow(v, 1 / 2.2) * 255), 0), 255);
 }
 
@@ -94,10 +96,10 @@ struct Sphere {
 		const double det = b * b - dot(op, op) + r * r;
 		if (det < 0) { return {}; }
 		
-		const double t1 = b + sqrt(det);
-		if (tmin < t1 && t1 < tmax) { return Hit{ t1, V(), V(), this }; }
-		const double t2 = b - sqrt(det);
-		if (tmin < t2 && t2 < tmax) { return Hit{ t2, V(), V(), this }; }
+		const double t1 = b - sqrt(det);
+		if (tmin < t1 && t1 < tmax) { return Hit{ t1, {}, {}, this }; }
+		const double t2 = b + sqrt(det);
+		if (tmin < t2 && t2 < tmax) { return Hit{ t2, {}, {}, this }; }
 		return {};
 	}
 };
@@ -105,7 +107,19 @@ struct Sphere {
 struct Scene {
 	// シーン中に存在する球
 	std::vector<Sphere> spheres{
-		{ V(), 1 }
+		// 1
+		//{ V(-.5, 0, 0), 1 },
+		//{ V( .5, 0, 0), 1 },
+
+		// Cornell Box
+		{ V(1e5 + 1, 40.8, 81.6), 1e5 },
+		{ V(-1e5 + 99, 40.8, 81.6), 1e5 },
+		{ V(50, 40.8, 1e5), 1e5 },
+		{ V(50, 1e5, 81.6), 1e5 },
+		{ V(50, -1e5 + 81.6, 81.6), 1e5 },
+		{ V(27, 16.5, 47), 16.5 },
+		{ V(73, 16.5, 78), 16.5 },
+		{ V(50, 681.6-.27, 81.6), 600 },
 	};
 
 	// 交差したならばHit型の値を返す、交差していない場合はstd::nullptrを返す
@@ -143,8 +157,29 @@ struct Scene {
 
 int main()
 {
+	// Image Size
 	const int w = 1200;
 	const int h = 800;
+
+	// Camera patameters
+	// 1
+	//const V eye(5, 5, 5);		// カメラ位置
+	//const V center(0, 0, 0);	// カメラ注視点
+	//const V up(0, 1, 0);		// 上方向の定義
+	//const double fov = 30 * M_PI/180;		// field of view
+
+	// Cornell Box
+	const V eye(50, 52, 295.6);		// カメラ位置
+	const V center = eye + V(0, -0.042612, -1);	// カメラ注視点
+	const V up(0, 1, 0);		// 上方向の定義
+	const double fov = 30 * M_PI / 180;		// field of view
+
+	const double aspect = (double)w / (double)h;// アスペクト比
+	// Basis vectors for camera coordinates
+	const auto wE = normarize(eye - center);	// カメラ向きの反対
+	const auto uE = normarize(cross(up, wE));
+	const auto vE = cross(wE, uE);
+
 	Scene scene;
 	std::ofstream ofs("result.ppm");
 	ofs << "P3\n" << w << " " << h << "\n255\n";
@@ -152,10 +187,19 @@ int main()
 
 		// Ray
 		const int x = i % w;	// x軸成分
-		const int y = i / w;	// y軸成分
+		const int y = h - i / w;	// y軸成分(上下反転)
 		Ray ray;
-		ray.o = V(2.*double(x) / w - 1, 2.*double(y) / h - 1, 5);
-		ray.d = V(0, 0, -1);
+		//ray.o = V(2.*double(x) / w - 1, 2.*double(y) / h - 1, 5);
+		//ray.d = V(0, 0, -1);
+		ray.o = eye;
+		ray.d = [&]() {
+			const double tf = std::tan(fov*.5);
+			const double rpx = 2.*x / w - 1;
+			const double rpy = 2.*y / h - 1;
+			const V w = normarize(
+				V(aspect*tf*rpx, tf*rpy, -1));	// アスペクト比・fovに合わせたカメラ座標（スクリーン座標）
+			return uE * w.x + vE * w.y + wE * w.z;	// ワールド座標に変換
+		}();
 
 		const auto h = scene.intersect(ray, 0, 1e+10);
 		if (h) {
